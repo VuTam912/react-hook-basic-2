b9d72fa74f5f18bd4ed7b58cfa2f90a08065bb83
Vu Tam R <vutam311291@gmail.com>
2023-09-03 22:04:54 +0700

    React-router-v5

--- src/App.js
+++ src/App.js
@@ -3,24 +3,49 @@ import './App.scss';
 import Home from './components/Home';
 import AddNewProduct from './components/AddNewProduct';
 import Product from './components/Products/Product';
+import Nav from './components/Navigation/Nav';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header content-left'>
-				<div style={{ textAlign: 'center' }}>
-					<img src={logo} className='App-logo' alt='logo' />
-				</div>
-				<p>
-					<span>RYO</span> with React Hook
-				</p>
-				<Home />
-			</header>
-			<div className='content-right'>
-				<AddNewProduct />
-				<hr />
-				<Product />
-			</div>
-		</div>
+		<Router>
+			<Nav />
+			{/* Xu ly điều hướng trang */}
+			<Switch>
+				<Route path='/' exact={true}>
+					<div className='App'>
+						<header className='App-header content-left'>
+							<div style={{ textAlign: 'center' }}>
+								<img src={logo} className='App-logo' alt='logo' />
+							</div>
+							<p>
+								<span>RYO</span> with React Hook
+							</p>
+							<Home />
+						</header>
+						<div className='content-right'>
+							<AddNewProduct />
+							<hr />
+							<Product />
+						</div>
+					</div>
+				</Route>
+				<Route path='/product'>
+					<Product />
+				</Route>
+				<Route path='/weather'>
+					<div>Weather App</div>
+				</Route>
+				<Route path='/about'>
+					<div>About</div>
+				</Route>
+				<Route path='*'>
+					<div>
+						<h2>404 Not Found</h2>
+					</div>
+				</Route>
+			</Switch>
+		</Router>
 	);
 }
 
